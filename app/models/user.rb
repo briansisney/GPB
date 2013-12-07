22958dc7d2934bea4e884d2c7cfcbeb45bb11e4f
MakerSquare Student <students@themakersquare.com>
2013-12-07 16:34:50 -0600

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
 	has_many :books
+	def self.from_omniauth(auth)
+	  where(auth.slice(:uid)).first_or_initialize.tap do |user|
+	    user.uid = auth.uid
+	    user.first_name = auth.info.first_name
+	    user.last_name = auth.info.last_name
+	    user.email = auth.info.email
+	    user.image = auth.info.image
+	    user.oauth_token = auth.credentials.token
+	    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+	    user.save!
+	  end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 GPB::Application.routes.draw do
-	# match 'auth/:provider/callback', to: 'sessions#create'
-	# match 'auth/failure', to: redirect('/')
-	# match 'signout', to: 'sessions#destroy', as: 'signout'
+	get 'auth/:provider/callback', to: 'sessions#create'
+	get 'auth/failure', to: redirect('/')
+	get 'signout', to: 'sessions#destroy', as: 'signout'
 
 	# get '/auth/facebook/callback', to: 'sessions#create'
   root 'books#index'
