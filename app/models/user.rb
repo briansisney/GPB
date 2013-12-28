4fd52606e59b6d7ad02749c7cc39af19e1dc26ab
MakerSquare Student <students@themakersquare.com>
2013-12-27 21:47:04 -0500

    Made the system of user points functional.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 	    user.image = auth.info.image
 	    user.oauth_token = auth.credentials.token
 	    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+	    user.points= 10
 	    user.save!
 	  end
 	end
