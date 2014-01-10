88ca9ac91d9c840587cf90965c8c49355546bea9
MakerSquare Student <students@themakersquare.com>
2014-01-09 23:20:58 -0600

    Create function that works with omni-auth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:current_user_id] = @user.id
+    flash[:notice] = "You have successfully created a new user and logged in!"
+    redirect_to @user
+  else
+    render 'new'
+  end
+  end
+
+
   def edit
     @user = current_user 
   end
@@ -20,17 +33,6 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(current_user.id)
   end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     session[:current_user_id] = @user.id
-  #     flash[:notice] = "You have successfully created a new user and logged in!"
-  #     redirect_to @user
-  #   else
-  #     render 'new'
-  #   end
-  # end
 
   def my_books
   @user = current_user.id   
